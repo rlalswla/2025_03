bf2a70ba669d7e1d8bcc19dc8f2c12a4f1373493
elect10 <wlgnsakswp@naver.com>
2025-03-20 14:04:04 +0900

    fix: metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,40 @@
-import type React from "react"
-import "@/app/globals.css"
-import { Inter } from "next/font/google"
-import { ThemeProvider } from "@/components/theme-provider"
-import { LanguageProvider } from "@/contexts/language-context"
+import type React from "react";
+import "@/app/globals.css";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
+import { LanguageProvider } from "@/contexts/language-context";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Your Name - Frontend Developer",
-  description: "Personal portfolio showcasing my projects and skills as a frontend developer",
-    generator: 'v0.dev'
-}
+  title: "Lee Jihoon portfolio",
+  description:
+    "Personal portfolio showcasing my projects and skills as a frontend developer",
+  generator: "Lee Jihoon",
+  icons: {
+    icon: "/profile-image.png",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
           <LanguageProvider>{children}</LanguageProvider>
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
 
-
-
-import './globals.css'
+import "./globals.css";
